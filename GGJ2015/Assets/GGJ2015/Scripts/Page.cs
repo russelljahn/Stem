6e76607ef52bc386ed3e75fdd66f3618b71abf13
russelljahn <russelljahn@gmail.com>
2015-01-24 14:32:28 -0800

    Base prefab for pages.

---
+++ GGJ2015/Assets/GGJ2015/Scripts/Page.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace Assets.GGJ2015.Scripts {
+    [RequireComponent(typeof(CanvasGroup))]
+    public class Page : MonoBehaviour {
+        public List<Text> _choicesTexts = new List<Text>();
+    }
+}
